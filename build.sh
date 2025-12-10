3f0aa37f2e7f55c35d3d499e03d0c0ee426dc170
Sethapat Yanapatsorn <6610685361@student.tu.ac.th>
2025-12-10 19:36:07 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py createsuperuser --username admin --email "admin@email.com" --noinput || true
